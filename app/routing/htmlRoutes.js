e2c0d87563e2d80e08394dd6b0645a5c585185c1
jonotann <jonathnrldn@gmail.com>
2018-08-15 23:53:50 -0400

    testing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,17 @@
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
+// Pull in required dependencies
+var path = require('path');
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-});
+// Export HTML routes
+module.exports = function(app) {
+	// console.log('___ENTER htmlRoutes.js___');
+
+	// Home page
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+
+	// Survey page
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
+};
--- server.js
+++ server.js
@@ -4,3 +4,20 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, '/app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, '/app/routing/apiRoutes'))(app);
+require(path.join(__dirname, '/app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Listening on PORT: ' + PORT);
+});
